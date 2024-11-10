78b06dc8964552d56e13c9f65741482ec0b627bd
Gabriela Gomes <gabrielaofgomes@gmail.com>
2024-11-10 19:57:41 -0300

    Atualizar README e implementar a classe ContaTerminal para entrada de dados do usuário

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite a agência: ");
+        String agenciaConta = scanner.next();
+
+        System.out.println("Por favor, digite a conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o saldo: ");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.println(
+            "Olá " + nomeCliente + 
+            ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaConta + 
+            ", conta " + numeroConta + 
+            " e seu saldo R$" + saldoConta + 
+            " já está disponível para saque."
+        );
     }
 }
